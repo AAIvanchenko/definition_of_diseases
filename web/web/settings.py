20114ada0847a6ce783496284331ab28bdc63841
Anastasia Ivanchenko <89zazaza89@gmail.com>
2022-05-24 22:34:17 +0700

    Добавление имени хоста в допустимые

--- web/web/settings.py
+++ web/web/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@*c6x&gsgh9xn6vfrf7g+_(b=*43-efk4sdasdaasd2$u0*1c_o@#jc6iuj)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ivanchenkoaa.pythonanywhere.com']
 
 
 # Application definition
